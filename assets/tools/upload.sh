7d4e3a6ea30ec6820ca8ff12398cf49ee1721749
Thomas L <wxyzzyrd@openmailbox.org>
2017-01-24 18:33:34 -0500

    Made uploader work over https

--- assets/tools/upload.sh
+++ assets/tools/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-URL=$(curl -s -F "apikey=YOURKEYHERE" -F "files[]=@$1" www.shimapan.rocks/upload.php | grep url | awk '{print $2}')
+URL=$(curl -s -F "apikey=YOURKEYHERE" -F "files[]=@$1" https://www.shimapan.rocks/upload.php | grep url | awk '{print $2}')
 echo $URL | tr -d '[\\\,"]'

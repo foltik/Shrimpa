582b1260a2f77f0b341a3cb0093deb6cbd395e14
Jack <jfoltzt@gmail.com>
2017-10-14 15:17:16 -0400

    Update gulpfile to separate apps

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,27 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-var jsFiles = 'public/js/**/*.js',
-	jsDest  = 'public/libs/app';
+gulp.task('js', function() {
+    gulp.start('shimapan');
+    gulp.start('shimapan-panel');
+});
 
-gulp.task('concat', function() {
-	return gulp.src(jsFiles)
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest(jsDest));
+gulp.task('shimapan', function() {
+	return gulp.src([
+	    'public/js/services/*.js',
+        'public/js/shimapan/**/*.js'
+    ])
+		.pipe(concat('shimapan.min.js'))
+		.pipe(uglify())
+        .pipe(gulp.dest('public/libs/app'));
+});
+
+gulp.task('shimapan-panel', function() {
+    return gulp.src([
+        'public/js/services/*.js',
+        'public/js/shimapan-panel/**/*.js'
+    ])
+        .pipe(concat('shimapan-panel.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/libs/app'));
 });

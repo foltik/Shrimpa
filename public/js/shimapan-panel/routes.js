ce951882856d6e950a55fc62bd20fa478671776d
Jack <jfoltzt@gmail.com>
2017-10-14 17:06:43 -0400

    Change href to ui-sref and make title a link

--- public/js/shimapan-panel/routes.js
+++ public/js/shimapan-panel/routes.js
@@ -4,11 +4,27 @@ angular.module('PanelRoutes', ['ui.router']).config(['$stateProvider', '$urlRout
     $urlRouterProvider.otherwise('/panel');
 
     $stateProvider
-        .state('/panel', {
+        .state('dashboard', {
             url: '/panel',
             templateUrl: '/views/shimapan-panel/panel-home.html'
-        }).state('/panel/api', {
-        url: '/panel/api',
+        }).state('search', {
+            url: '/panel/search',
+            templateUrl: '/views/shimapan-panel/panel-search.html'
+        }).state('api', {
+            url: '/panel/api',
             templateUrl: '/views/shimapan-panel/panel-api.html'
+        }).state('invites', {
+            url: '/panel/invites',
+            templateUrl: '/views/shimapan-panel/panel-invites.html'
+        }).state('stats', {
+            url: '/panel/stats',
+            templateUrl: '/views/shimapan-panel/panel-stats.html'
+        }).state('users', {
+            url: '/panel/users',
+            templateUrl: '/views/shimapan-panel/panel-users.html'
+        }).state('home', {
+            onEnter: ['$window', function($window) {
+                $window.location.href = '/home';
+            }]
         });
 }]);

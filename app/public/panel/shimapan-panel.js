1d2987bf22f7e2fd4b93e5a2d63ce57f750020d1
Jack Foltz <jack@foltz.io>
2018-01-14 10:27:37 -0500

    Add base for invite creation/viewing page

--- app/public/panel/shimapan-panel.js
+++ app/public/panel/shimapan-panel.js
@@ -1,6 +1,6 @@
 var angular = require('angular');
 var uirouter = require('angular-ui-router');
-var app = angular.module('shimapan-panel', ['ui.router', 'AuthSvc', 'ApiSvc', 'ApiCtrl', 'NavCtrl', 'PanelRoutes']);
+var app = angular.module('shimapan-panel', ['ui.router', 'AuthSvc', 'ApiSvc', 'InviteSvc', 'ApiCtrl', 'InviteCtrl', 'NavCtrl', 'PanelRoutes']);
 
 app.run(['$rootScope', '$state', '$stateParams', function($rootScope, $state, $stateParams) {
     $rootScope.$state = $state;

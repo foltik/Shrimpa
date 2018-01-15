150b7360710ec880923f0f843826d6ede1419e70
Jack Foltz <jack@foltz.io>
2018-01-15 18:14:37 -0500

    Add unit shortening to users page

--- app/public/panel/controllers/UserCtrl.js
+++ app/public/panel/controllers/UserCtrl.js
@@ -6,4 +6,21 @@ angular.module('UserCtrl', ['UserSvc']).controller('UserController', ['$scope', 
             $scope.users = users;
         });
     };
+
+    $scope.shorten = function(size) {
+        var units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        var currUnit = 0;
+
+        var num = parseFloat(size);
+        for (i = 0; i < units.length; i++) {
+            if (num / 1000 >= 1 && currUnit < units.length - 1) {
+                currUnit++;
+                num /= 1000;
+            } else {
+                // Round to 2 decimal places
+                return Number(Math.round(num + 'e1')+'e-1') + ' ' +
+                    units[currUnit];
+            }
+        }
+    }
 }]);

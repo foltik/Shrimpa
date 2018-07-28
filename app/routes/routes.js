28555cff3f521c152a186a1afcf543f127cce15c
Jack Foltz <jack@foltz.io>
2018-07-28 17:13:50 -0400

    Remove auth checking from routes.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,66 +1,26 @@
-var index = require('./index.js');
-var home = require('./home.js');
-var upload = require('./upload.js');
-var view = require('./view.js');
-var auth = require('./auth.js');
-var register = require('./register.js');
-var login = require('./login.js');
-var panel = require('./panel.js');
-var keys = require('./keys.js');
-var invites = require('./invites.js');
-var users = require('./users.js');
-
-var Key = require('../models/Key.js');
-
-var checkApiKey = function (key, cb) {
-    Key.find({key: key}, function (err, res) {
-        if (err) throw err;
-        cb(res.length === 1);
-    });
-};
-
-var requireLogin = function (req, res, next) {
-    if (!req.session || !req.session.passport)
-        return res.redirect('/login');
-    else
-        return next();
-};
-
-var requireLoginApi = function(req, res, next) {
-    if (!req.session || !req.session.passport) {
-        if (!req.body.apikey) {
-            return res.redirect('/login');
-        } else {
-            checkApiKey(res.body.apikey, function(valid) {
-                if (!valid)
-                    return res.sendStatus(401);
-                else
-                    return next();
-            });
-        }
-    } else {
-        return next();
-    }
-};
+const index = require('./index.js');
+const home = require('./home.js');
+const upload = require('./upload.js');
+const view = require('./view.js');
+const auth = require('./auth.js');
+const register = require('./register.js');
+const login = require('./login.js');
+const panel = require('./panel.js');
+const keys = require('./keys.js');
+const invites = require('./invite.js');
+const users = require('./users.js');
 
 module.exports = function (app) {
     app.use('/', index);
-    app.use('/home', requireLogin, home);
+    app.use('/home', home);
     app.use('/v', view);
     app.use('/api/upload', upload);
     app.use('/api/auth', auth);
-    app.use('/api/keys', requireLogin, keys);
-    app.use('/api/invites', requireLogin, invites);
-    app.use('/api/users', requireLogin, users);
+    app.use('/api/keys', keys);
+    app.use('/api/invites', invites);
+    app.use('/api/users', users);
     app.use('/register', register);
     app.use('/login', login);
-    app.use('/panel', requireLogin, panel);
-    app.use('/panel*', requireLogin, panel);
-
-    app.use(function (err, req, res) {
-        if (err.name === 'UnauthorizedError') {
-            res.status(401);
-            res.json({"message": err.name + ": " + err.message});
-        }
-    })
+    app.use('/panel', panel);
+    app.use('/panel*', panel);
 };

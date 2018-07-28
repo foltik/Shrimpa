740c153093e71197c8f7530795531f0158c4347c
Jack Foltz <jack@foltz.io>
2018-07-28 16:49:54 -0400

    create verifyBody middleware factory to abstract request validation

---
+++ app/util/verifyBody.js
@@ -0,0 +1,23 @@
+
+// Verifies the request body is well formed
+// expectedProps follows the format:
+// [{name: 'myList', instance: 'Array'}, {name: 'myVar', type: 'string', optional: true}, etc.]
+const verifyBody = expectedProps =>
+    (req, res, next) => {
+        for (let i = 0; i < expectedProps.length; i++) {
+            const expected = expectedProps[i];
+            const prop = req.body[expected.name];
+
+            if (!expected.optional && !prop)
+                return res.status(400).json({message: expected.name + ' not specified.'});
+
+            if (expected.type && typeof prop !== expected.type)
+                return res.status(400).json({message: expected.name + ' malformed.'});
+
+            if (expected.instance && !(prop instanceof expected.instance))
+                return res.status(400).json({message: expected.name + ' malformed.'});
+        }
+        next();
+    };
+
+module.exports = verifyBody;

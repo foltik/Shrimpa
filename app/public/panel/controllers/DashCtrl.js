9e7679e53980beaab06cce5eeb483ed1f840e571
Jack Foltz <jack@foltz.io>
2018-08-15 11:23:30 -0400

    Add graphs to dashboard

--- app/public/panel/controllers/DashCtrl.js
+++ app/public/panel/controllers/DashCtrl.js
@@ -1,23 +1,35 @@
 var angular = require('angular');
 
 angular.module('DashCtrl', ['chart.js']).controller('DashController', ['$scope', $scope => {
-    $scope.uploadColors = ['#2a9fd6'];
-    $scope.uploadLabels = ["January", "February", "March", "April", "May", "June", "July"];
+    const colorScheme = ['#2a9fd6'];
+
+    let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for(let i = 0; i < 6 - (new Date()).getDay(); i++) {
+        const back = weekDays.pop();
+        weekDays.unshift(back);
+    }
+
+    $scope.uploadColors = colorScheme;
+    $scope.uploadLabels = weekDays;
     $scope.uploadSeries = ['Uploads'];
-    $scope.uploadData = [[65, 59, 80, 81, 56, 55, 40]];
-    $scope.uploadDatasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+    $scope.uploadData = [[10, 20, 30, 20, 15, 20, 45]];
     $scope.uploadOptions = {
-        scales: {
-            yAxes: [
-                {
-                    id: 'y-axis-1',
-                    type: 'linear',
-                    display: true,
-                    position: 'left'
-                }
-            ]
-        }
+        title: {
+            display: true,
+            text: 'Historical Uploads'
+        },
     };
 
-
+    $scope.viewColors = colorScheme;
+    $scope.viewLabels = weekDays;
+    $scope.viewSeries = ['Views'];
+    $scope.viewData = [[5, 11, 4, 3, 7, 9, 21]];
+    $scope.viewOptions = {
+        title: {
+            display: true,
+            text: 'Historical Views'
+        }
+    };
 }]);
+{
+}

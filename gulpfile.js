013f8971cdd951f46b5e00873211d2e18fad7054
Jack Foltz <jack@foltz.io>
2018-12-31 12:16:22 -0500

    Unify minify browserify streams with sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
 'use strict';
 
 const gulp = require('gulp');
-const concat = require('gulp-concat');
 const rename = require('gulp-rename');
-//const uglify = require('gulp-uglify');
-const uglifyes = require('uglify-es');
-const composer = require('gulp-uglify/composer');
-const uglify = composer(uglifyes, console);
+const uglify = require('gulp-uglify-es').default;
 const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 const browserify = require('browserify');
 const es = require('event-stream');
 const cleanCSS = require('gulp-clean-css');
 const nodemon = require('gulp-nodemon');
+const sourcemaps = require('gulp-sourcemaps');
 const path = require('path');
+const glob = require('glob');
 
-gulp.task('start', (done) => {
+gulp.task('start', done => {
     nodemon({
         script: 'server.js',
         ignore: '*.*'
@@ -22,113 +21,83 @@ gulp.task('start', (done) => {
     done();
 });
 
-gulp.task('watch', (done) => {
+gulp.task('watch', () => {
+    const fileTasks = new Map([
+        ['.js', 'BuildJS'],
+        ['.css', 'BuildCSS']
+    ]);
+
     nodemon({
         script: 'server.js',
-        ext: 'js html css',
+        ext: 'js css',
         env: {'NODE_ENV': 'dev'},
         watch: [
             'app/',
             'config/'
         ],
-        tasks: function (changedFiles) {
-            var tasks = [];
-            changedFiles.forEach(function (file) {
-                if (path.extname(file) === '.js' && !~tasks.indexOf('MakeJS'))
-                    tasks.push('MakeJS');
-                if (path.extname(file) === '.css' && !~tasks.indexOf('MakeCSS'))
-                    tasks.push('MakeCSS');
-            });
-            return tasks;
-        }
-    }).on('restart?', function () {
-        gulp.task('default')();
-    });
+        tasks: changedFiles =>
+            changedFiles
+                .map(file => fileTasks.get(path.extname(file)))
+                .filter((value, index, self) => self.indexOf(value) === index)
+    }).on('restart?', 'default')
 });
 
-gulp.task('MinifyCSS', () => {
-    const files = [
-        {
-            src: 'app/public/css/form.css',
-            dest: 'form.min.css'
-        },
-        {
-            src: 'app/public/css/home.css',
-            dest: 'home.min.css'
-        },
-        {
-            src: 'app/public/css/panel.css',
-            dest: 'panel.min.css'
-        },
-        {
-            src: 'app/public/css/index.css',
-            dest: 'index.min.css'
-        }
-    ];
+gulp.task('BuildCSS', () => {
+    const files = [{
+        src: 'app/public/css/form.css',
+        dest: 'form.min.css'
+    }, {
+        src: 'app/public/css/home.css',
+        dest: 'home.min.css'
+    }, {
+        src: 'app/public/css/panel.css',
+        dest: 'panel.min.css'
+    }, {
+        src: 'app/public/css/index.css',
+        dest: 'index.min.css'
+    }];
 
     const tasks = files.map(file =>
         gulp.src(file.src)
             .pipe(cleanCSS())
-            .pipe(rename(file.dest))
-            .pipe(gulp.dest('public/css')));
+            .pipe(rename(file.dest)));
 
     return es.merge(tasks)
         .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('MinifyJS', () => {
-    const files = [
-        {
-            src: [
-                'app/public/services/*.js',
-                'app/public/panel/**/*.js'
-            ],
-            dest: 'panel.min.js'
-        },
-        {
-            src: [
-                'app/public/services/*.js',
-                'app/public/shimapan/**/*.js'
-            ],
-            dest: 'shimapan.min.js'
-        },
-        {
-            src: 'app/public/index/*.js',
-            dest: 'index.min.js'
-        }
-    ];
+gulp.task('BuildJS', () => {
+    const files = [{
+        src: [
+            'app/public/services/*.js',
+            'app/public/panel/**/*.js'
+        ],
+        dest: 'panel.bundle.js'
+    }, {
+        src: [
+            'app/public/services/*.js',
+            'app/public/shimapan/**/*.js'
+        ],
+        dest: 'shimapan.bundle.js'
+    }, {
+        src: [
+            'app/public/index/*.js'
+        ],
+        dest: 'index.bundle.js'
+    }];
 
     const tasks = files.map(file =>
-        gulp.src(file.src)
-            .pipe(concat(file.dest))
+        browserify({entries: file.src.map(g => glob.sync(g)), debug: true})
+            .bundle()
+            .pipe(source(file.dest))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
             .pipe(uglify())
-    );
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('public/js')));
 
     return es.merge(tasks)
         .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('BrowserifyJS', () => {
-    const files = [
-        {
-            src: 'public/js/shimapan.min.js',
-            dest: 'shimapan.bundle.js'
-        }, {
-            src: 'public/js/panel.min.js',
-            dest: 'panel.bundle.js'
-        }
-    ];
-
-    const tasks = files.map(entry =>
-        browserify({entries: [entry.src], debug: true}).bundle()
-            .pipe(source(entry.dest))
-    );
-
-    return es.merge(tasks)
-        .pipe(gulp.dest('public/js'));
-});
-
-gulp.task('MakeJS', gulp.series('MinifyJS', 'BrowserifyJS'));
-gulp.task('MakeCSS', gulp.series('MinifyCSS'));
-
-gulp.task('default', gulp.parallel('MakeJS', 'MakeCSS'));
+gulp.task('default', gulp.parallel('BuildJS', 'BuildCSS'));
